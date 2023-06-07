51756b818f49edd1594026cae6233a57b4323246
faimin <fuxianchao@gmail.com>
2023-06-08 01:41:19 +0800

    add prefix

--- ZDToolBoxObjC/Classes/ZDCategory/Foundation/NSTimer+ZDUtility.m
+++ ZDToolBoxObjC/Classes/ZDCategory/Foundation/NSTimer+ZDUtility.m
@@ -20,16 +20,16 @@ ZD_AVOID_ALL_LOAD_FLAG_FOR_CATEGORY(NSTimer_ZDUtility)
 #pragma mark - Public Method
 
 + (NSTimer *)zd_scheduledTimerWithTimeInterval:(NSTimeInterval)seconds repeats:(BOOL)repeats block:(void (^)(NSTimer *timer))block {
-    return [NSTimer scheduledTimerWithTimeInterval:seconds target:self selector:@selector(executeTimerBlock:) userInfo:[block copy] repeats:repeats];
+    return [NSTimer scheduledTimerWithTimeInterval:seconds target:self selector:@selector(zd_executeTimerBlock:) userInfo:[block copy] repeats:repeats];
 }
 
 + (NSTimer *)zd_timerWithTimeInterval:(NSTimeInterval)seconds repeats:(BOOL)repeats block:(void (^)(NSTimer *timer))block {
-    return [NSTimer timerWithTimeInterval:seconds target:self selector:@selector(executeTimerBlock:) userInfo:[block copy] repeats:repeats];
+    return [NSTimer timerWithTimeInterval:seconds target:self selector:@selector(zd_executeTimerBlock:) userInfo:[block copy] repeats:repeats];
 }
 
 #pragma mark - Private Method
 
-+ (void)executeTimerBlock:(NSTimer *)timer {
++ (void)zd_executeTimerBlock:(NSTimer *)timer {
     if ([timer userInfo]) {
         void(^timerBlock)(NSTimer *) = (void(^)(NSTimer *))[timer userInfo];
         timerBlock(timer);
